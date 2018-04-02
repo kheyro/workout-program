fcdcceada319ad952b331dda05a233a908c346f0
Denis Atkesone <denis.atkesone@gmail.com>
2018-04-02 15:56:13 +0200

    Configure simple routes

--- app/models/program.rb
+++ app/models/program.rb
@@ -1,5 +1,5 @@
 class Program < ApplicationRecord
-  has_many :workouts
+  has_many :workouts, :dependent => :delete_all
   has_many :exercises, through: :workouts
 
   validates :name, presence: true, uniqueness: { case_sensitive: false }
