ad42d56542d1ca850320a8383c35c69e6660ecba
Denis Atkesone <denis.atkesone@gmail.com>
2018-04-04 14:14:52 +0200

    Adding bootstrap

--- client/src/index.js
+++ client/src/index.js
@@ -5,6 +5,7 @@ import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 import { Provider } from 'react-redux'
 import { store } from './store'
+import 'bootstrap/dist/css/bootstrap.css';
 
 ReactDOM.render(
   <Provider store={store}>
