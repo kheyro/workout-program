9de51ed6bbce84bfc5a5a1e15ecd80945342fc0d
Denis Atkesone <denis.atkesone@gmail.com>
2018-04-04 16:24:29 +0200

    Fix typo on welcome page

--- client/src/components/Welcome.js
+++ client/src/components/Welcome.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Welcome = () =>
   <div className="row align-items-center h-100">
     <div className="col">
-      <h1 className="text-center">Welcome to you workout planner</h1>
+      <h1 className="text-center">Welcome to your My Workout Planner</h1>
     </div>
   </div>;
 
